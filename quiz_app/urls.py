3c516aa2bb75f6df4b43f77c2e3d73ddcd984c59
Анохин Павел <anokhin.p@nt351.a1tis.ru>
2022-11-28 06:39:45 +0300

    created list fo questions

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from .views import TestListView, TestDetail
+from .views import TestListView, TestView
 
 
 urlpatterns = [
 	path('', TestListView.as_view(), name='TestList'),
-	path('test/<int:pk>', TestDetail.as_view(), name='TestDetail') 
+	path('test/<int:pk>', TestView.as_view(), name='TestView') 
 ]
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Test
+from .models import Test, Question
 
 class TestListView(ListView):
     model = Test
-    paginate_by = 1
+    
 
-class TestDetail(DetailView):
-	model = Test
+class TestView(ListView):
+	model = Question
+	template_name= 'quiz_app/questions.html'
+	paginate_by = 1
+
+	def get_queryset(self):
+		queryset = Test.objects.get(pk=self.kwargs['pk']).questions.all()
+		return queryset
 
     
